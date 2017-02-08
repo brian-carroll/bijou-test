b1cc81d821b684b2e609af82c598f940099a2512
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-02-08 00:21:35 +0000

    Get sqlite database set up

--- flask/scraper/scraper.py
+++ flask/scraper/scraper.py
@@ -19,6 +19,29 @@ def connect_db():
     return rv
 
 
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.before_first_request
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route('/', methods=['GET'])
 def home():
